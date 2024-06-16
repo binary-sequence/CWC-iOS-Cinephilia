2ab512e234ee07d31667ebb60b40156609a6576a
Sergio Lindo Mansilla <sergiolindo.empresa@gmail.com>
2024-06-16 21:30:13 +0200

    Add template Xcode project

---
+++ Cinephilia/Cinephilia/CinephiliaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinephiliaApp.swift
+//  Cinephilia
+//
+//  Created by Sergio Lindo Mansilla on 16.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct CinephiliaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
